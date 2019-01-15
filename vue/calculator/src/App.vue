ede6496f0abd0fe34318781264d13ea4d77a6071
manjor <manoeltavares.jr@gmail.com>
2019-01-15 16:37:53 -0300

    Criando Projeto

---
+++ vue/calculator/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <h1>Olá Vue!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "RobotoRegular";
+    src: url("./fonts/Roboto-Regular.ttf")
+}
+* {
+    font-family: "RobotoRegular",monospace;
+}
+
+body{
+    margin: 0;
+}
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: white;
+    background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);
+}
+
+</style>
